f222598a838f4f1b17ccde763b5ab2cf42079977
Rack Lin <racklin@gmail.com>
2014-11-29 00:41:37 +0800

    Fixed linux version run-xulapp.sh will not launch xulapp bugs.
    Signed-off-by: Rack Lin <racklin@gmail.com>

--- build.sh
+++ build.sh
@@ -341,15 +341,15 @@ if [ $BUILD_LINUX == 1 ]; then
 		# Merge xulrunner and relevant assets
 		cp -R "$BUILDDIR/"* "$APPDIR"
 		cp -r "$RUNTIME_PATH" "$APPDIR/xulrunner"
-		mv "$APPDIR/xulrunner/xulrunner-stub" "$APPDIR/$MODULE"
-		chmod 755 "$APPDIR/$MODULE"
+		mv "$APPDIR/xulrunner/xulrunner-stub" "$APPDIR/$MODULE-bin"
+		chmod 755 "$APPDIR/$MODULE-bin"
 
 		# Delete extraneous files
 		find "$APPDIR" -depth -type d -name .git -exec rm -rf {} \;
 		find "$APPDIR" \( -name .DS_Store -or -name update.rdf \) -exec rm -f {} \;
 
 		# Add run-$MODULE.sh
-		cp "$CALLDIR/linux/run-$MODULE.sh" "$APPDIR/run-$MODULE.sh"
+		cp "$CALLDIR/linux/run-$MODULE.sh" "$APPDIR/$MODULE"
 
 		# Move icons, so that updater.png doesn't fail
 		mv "$APPDIR/xulrunner/icons" "$APPDIR/icons"
